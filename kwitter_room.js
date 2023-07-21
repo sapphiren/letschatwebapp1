4bc925ce201c8f956f0749c7c468c1f128b73e2c
sapphiren <117869067+sapphiren@users.noreply.github.com>
2023-07-21 18:57:47 -0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBbdu7rhCuI6W4sIAQA6brbpDSWAf9fAj8",
+    authDomain: "letschat-377f9.firebaseapp.com",
+    databaseURL: "https://letschat-377f9-default-rtdb.firebaseio.com",
+    projectId: "letschat-377f9",
+    storageBucket: "letschat-377f9.appspot.com",
+    messagingSenderId: "678841514196",
+    appId: "1:678841514196:web:8b075d57cd306a9d8b6341"
+  };
+  
+  // Initialize Firebase
+  var app = initializeApp(firebaseConfig);
